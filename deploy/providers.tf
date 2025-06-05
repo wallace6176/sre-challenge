3a5cd9a5d0635c976a049b0e372a1e9612256006
Wallace Gaturu <wallace@wallace-gaturu.com>
2025-06-05 16:38:42 +0200

    fix (#8)
    * reorganized app, removed unneeded requirements
    
    * added Dockerfile, Terraform and Helm deployment code
    
    * adding Actions workflow
    
    * workflow fix
    
    * workflow fix
    
    * workflow fix
    
    * pipeline fix
    
    * terraform deploy fixes
    
    * typo fix
    
    * quick fix
    
    * Terraform variable
    
    * pipeline fix
    
    * fix
    
    * env for terraform steps
    
    * fix
    
    * fix
    
    * eks authentication
    
    * fix
    
    * eks auth

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -4,13 +4,14 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.eneco.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eneco.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eneco.name]
-      command     = "aws"
-    }
+    config_path = "~/.kube/config"
+    # host                   = data.aws_eks_cluster.eneco.endpoint
+    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.eneco.certificate_authority.0.data)
+    # exec {
+    #   api_version = "client.authentication.k8s.io/v1beta1"
+    #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eneco.name]
+    #   command     = "aws"
+    # }
   }
 }
 
